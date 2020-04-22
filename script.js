88e326a3fa92510f50f0a5419daddd695eb4095f
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2020-04-22 14:27:29 +0300

    final-final

--- script.js
+++ script.js
@@ -9,21 +9,36 @@ function DomElement(selector, height, width, bg, fontSize){
 }
 
 DomElement.prototype.addNewElem = function(){
+    const block = document.querySelector('.block');
     let newElem;
     if(this.selector.startsWith('.')){
         newElem = document.createElement('div');
-        newElem.setAttribute('class', this.selector);
+        // начинаем class, удаляя точку благодаря substr(1)
+        newElem.setAttribute('class', this.selector.substr(1));
+        newElem.style.width = this.width;
+        newElem.style.height = this.height;
+        newElem.style.background = this.bg;
+        newElem.style.fontSize = this.fontSize;
+        newElem.textContent = this.selector;
+        block.appendChild(newElem);
     } else if (this.selector.startsWith('#')){
-        newElem = document.createElement('div');
-        newElem.setAttribute('id', this.selector);
+        newElem = document.createElement('p');
+            // начинаем class, удаляя точку благодаря substr(1)
+        newElem.setAttribute('id', this.selector.substr(1));
+        newElem.style.width = this.width;
+        newElem.style.height = this.height;
+        newElem.style.background = this.bg;
+        newElem.style.fontSize = this.fontSize;
+        newElem.textContent = this.selector;
+        block.appendChild(newElem);
     } else if (this.selector == null || this.selector == '' || !this.selector){
         return;
     }
 };
 
-const firstElement = new DomElement('#id', 'height: 50px', 'width: 300px', 'background-color: #f6f8fa', 'font-size: 15px'),
- secondElement = new DomElement('.class', 'height: 50px', 'width: 3100px', 'background-color: red', 'font-size: 15px'),
- thirdElement = new DomElement('null', 'height: 50px', 'width: 3100px', 'background-color: red', 'font-size: 15px');
+const firstElement = new DomElement('#id', '50px', '310px', 'blue', '15px'),
+ secondElement = new DomElement('.class', '50px', '310px', 'red', '15px'),
+ thirdElement = new DomElement('null', '50px', '310px', 'black', '15px');
  
 firstElement.addNewElem();
 secondElement.addNewElem();
